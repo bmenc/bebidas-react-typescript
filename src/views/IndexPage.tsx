58b62dc278df42a20494beb36fc5ca95068a0149
bmenc <bmenchaca.cca@gmail.com>
2026-03-08 14:31:46 -0600

    315. Múltiples Stores en Zustand con Slices Pattern

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,5 +1,8 @@
+import { useAppStore } from "../stores/useAppStore"
 
 export default function IndexPage() {
+
+  // useAppStore((state => state.catgories))
   return (
     <>
       <h1>Inicio</h1>
