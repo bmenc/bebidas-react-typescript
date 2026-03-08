8d3a913f0341cadd65538bde0818d2a8d47391f3
bmenc <bmenchaca.cca@gmail.com>
2026-03-08 12:43:44 -0600

    310. Agregando Navegación entre páginas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link, NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="bg-slate-800">
@@ -6,8 +8,15 @@ export default function Header() {
           <div>
             <img src="/logo.svg" alt="logotipo" className="w-32" />
           </div>
-          <nav>
-
+          <nav className="flex gap-4 ">
+            <Link
+              to="/"
+              className="text-white uppercase font-bold"
+            >Inicio</Link>
+            <Link
+              to="/favoritos"
+              className="text-white uppercase font-bold"
+            >Favoritos</Link>
           </nav>
         </div>
       </div>
