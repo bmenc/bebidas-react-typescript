7a39640136daee1837d16f22f40c4c93ecc2389f
bmenc <bmenchaca.cca@gmail.com>
2026-03-08 16:43:57 -0600

    328. Consultando la API para obtener los detalles de la receta por I D

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import { CategoriesAPIResponseSchema, DrinksAPIResponse, DrinkAPIResponse } from '../utils/recipes-schema'
-import type { SearchFilter } from '../types'
+import { CategoriesAPIResponseSchema, DrinksAPIResponse, DrinkAPIResponse, RecipeAPIResponseSchema } from '../utils/recipes-schema'
+import type { Drink, SearchFilter } from '../types'
 
 export async function getCategories() {
   const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
@@ -15,12 +15,17 @@ export async function getRecipes(filters: SearchFilter) {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}&i=${filters.ingredient}`
   const { data } = await axios(url)
   const result = DrinksAPIResponse.safeParse(data)
-  console.log(result);
-
   if (result.success) {
     return result.data
   }
 }
 
 
-// www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
+export async function getRecipeById(id: Drink['idDrink']) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+  const { data } = await axios(url)
+  const result = RecipeAPIResponseSchema.safeParse(data.drinks[0])
+  if (result.success) {
+    return result.data
+  }
+}
