753253fe33e5706422008b37a81450098668f17e
bmenc <bmenchaca.cca@gmail.com>
2026-03-08 15:55:29 -0600

    323. Buscar las Recetas en base a categoría e Ingredientes

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,5 @@
 import type { StateCreator } from "zustand"
-import { getCategories } from "../services/RecipeService"
+import { getCategories, getRecipes } from "../services/RecipeService"
 import type { Categories, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
@@ -19,7 +19,7 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     }
   },
   searchRecipes: async (filters) => {
-    console.log('desde RecipesSlice', filters);
+    await getRecipes(filters)
 
   }
 })
