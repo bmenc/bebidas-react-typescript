3972cbb50e3333c3207e3eefdb22827703d9f7a4
bmenc <bmenchaca.cca@gmail.com>
2026-03-14 12:13:46 -0600

    334. Creando un Slice para favoritos

---
+++ src/stores/favoritesSlice.ts
@@ -0,0 +1,10 @@
+import { StateCreator } from 'zustand'
+import type { Recipe } from '../types'
+
+export type FavoritesSliceType = {
+  favorites: Recipe[]
+}
+
+export const createFavoritesSlice: StateCreator<FavoritesSliceType> = () => ({
+  favorites: []
+})
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,9 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createRecipesSlice, type RecipesSliceType } from './recipeSlice'
+import { type FavoritesSliceType, createFavoritesSlice } from './favoritesSlice'
 
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-  ...createRecipesSlice(...a)
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+  ...createRecipesSlice(...a),
+  ...createFavoritesSlice(...a),
 })))
