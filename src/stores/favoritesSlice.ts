6f143915b3ba2ad4ec5c8ea2a977c522d2f2c766
bmenc <bmenchaca.cca@gmail.com>
2026-03-14 21:17:59 -0600

    337. Consumir Estado de otra Slice

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,13 +1,15 @@
 import { StateCreator } from 'zustand'
 import type { Recipe } from '../types'
+import { createRecipesSlice, type RecipesSliceType } from './recipeSlice'
 
 export type FavoritesSliceType = {
   favorites: Recipe[],
   handleClickFavorite: (recipe: Recipe) => void
   favoriteExists: (id: Recipe['idDrink']) => boolean
+
 }
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
+export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipesSliceType, [], [], FavoritesSliceType> = (set, get, api) => ({
   favorites: [],
   handleClickFavorite: (recipe) => {
     if (get().favoriteExists(recipe.idDrink)) {
@@ -19,6 +21,7 @@ export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get)
         favorites: [...state.favorites, recipe]
       }))
     }
+    createRecipesSlice(set, get, api).closeModal()
   },
   favoriteExists: (id) => {
     return get().favorites.some(favorite => favorite.idDrink === id)
